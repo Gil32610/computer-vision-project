d87832110474fa3b9096d4ca5bd06072fe04599c
GIL32610 <gil.00000032610@unicap.br>
2026-04-15 22:58:57 -0300

    fix init commands for rebuild

--- init-mobile.sh
+++ init-mobile.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 cd mobile
+
+chmod -R 777 .
 # 1. Download the wrapper files if they don't exist
 if [ ! -f "gradlew" ]; then
     mkdir -p gradle/wrapper
-    wget https://raw.githubusercontent.com/gradle/gradle/v8.5.0/gradle/wrapper/gradle-wrapper.jar -P gradle/wrapper/
-    wget https://raw.githubusercontent.com/gradle/gradle/v8.5.0/gradle/wrapper/gradle-wrapper.properties -P gradle/wrapper/
+    wget https://raw.githubusercontent.com/gradle/gradle/v8.5.0/gradle/wrapper/gradle-wrapper.jar -O gradle/wrapper/gradle-wrapper.jar
+    wget https://raw.githubusercontent.com/gradle/gradle/v8.5.0/gradle/wrapper/gradle-wrapper.properties -O gradle/wrapper/gradle-wrapper.properties
     curl -sL https://raw.githubusercontent.com/gradle/gradle/v8.5.0/gradlew -o gradlew
     chmod +x gradlew
 fi
@@ -15,4 +17,6 @@ echo "sdk.dir=/opt/android-sdk" > local.properties
 yes | sdkmanager "build-tools;34.0.0" "platforms;android-34"
 
 # 3. Trigger the first sync
+
+./gradlew clean 
 ./gradlew help
